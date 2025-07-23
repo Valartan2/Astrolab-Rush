a0de554d95ff4f3431f81f7870f493a663a6a13f
Valartan2 <anthony.maria19@gmail.com>
2025-07-24 01:00:58 +0200

    Update game.js

--- game.js
+++ game.js
@@ -103,7 +103,7 @@ function updateObjectifDisplay() {
   let hasReached1km = false;
 
   const distanceSpeedFactor = 2.5;
-  const CONSTANT_SPEED = 20;
+  const CONSTANT_SPEED = 19;
 
   /* -------------------- Stars Background -------------------- */
   const stars = Array.from({ length: 150 }, () => ({
