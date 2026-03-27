3a470d6f8b8b2fc03ce48afec4e8e4feb6f3b8ad
Valartan2 <anthony.maria19@gmail.com>
2026-03-27 21:50:50 +0100

    Update game.js

--- game.js
+++ game.js
@@ -2064,7 +2064,7 @@ progressLabel.style.display = "none";
 
 const GAME_SETTINGS = {
   endless: {
-    mobile: { baseSpeed: 11, accel: 1.1, step: 500, maxDist: 2500 },
+    mobile: { baseSpeed: 11, accel: 1.1, step: 500, maxDist: 2000 },
     desktop: { baseSpeed: 11, accel: 1.1, step: 500, maxDist: 3000 }
   },
   mission: {
