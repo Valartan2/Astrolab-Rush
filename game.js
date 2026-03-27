1400cb6023c8c782d3e3ace1511641343d9e8080
Valartan2 <anthony.maria19@gmail.com>
2026-03-27 14:25:11 +0100

    Update game.js

--- game.js
+++ game.js
@@ -2098,7 +2098,7 @@ function gameLoop(timestamp) {
 const GAME_SETTINGS = {
   endless: {
     mobile: { baseSpeed: 10, accel: 1.1, step: 500, maxDist: 4000 },
-    desktop: { baseSpeed: 11, accel: 0.8, step: 500, maxDist: 3000 }
+    desktop: { baseSpeed: 11, accel: 1.1, step: 500, maxDist: 3000 }
   },
   mission: {
     mobile: { baseSpeed: 9, accel: 0.4, step: 600, maxDist: 3000 },
