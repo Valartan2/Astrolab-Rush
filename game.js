d10fd28b80bacc6ba060fe4ef653f9fc13e4d89b
Valartan2 <anthony.maria19@gmail.com>
2026-04-14 20:59:30 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -2256,9 +2256,8 @@ toggleMusicBtn.onclick = () => {
 
         const WEBHOOK_URL = "https://hook.eu2.make.com/1omb34nsoy756rf6n7pqsk3a1yo73bmg";
 
-        await fetch(WEBHOOK_URL, {
+        const response = await fetch(WEBHOOK_URL, {
           method: "POST",
-          mode: "no-cors",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
             date: new Date().toISOString(),
@@ -2266,8 +2265,6 @@ toggleMusicBtn.onclick = () => {
             score: cumulBurn
           })
         });
-
-        const response = { ok: true };
 
         if (!response.ok) throw new Error("Webhook failed");
 
